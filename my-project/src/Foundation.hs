ca652d55e31561ab58215ab5ef2481e36fc45fc5
hukasu <lucas_fnf@hotmail.com>
2018-02-26 21:31:35 -0300

    Changing from bower to yarn

--- my-project/src/Foundation.hs
+++ my-project/src/Foundation.hs
@@ -189,6 +189,7 @@ instance YesodBreadcrumbs App where
   breadcrumb HomeR = return ("Home", Nothing)
   breadcrumb (AuthR _) = return ("Login", Just HomeR)
   breadcrumb ProfileR = return ("Profile", Just HomeR)
+  breadcrumb CronogramaR = return ("Login", Just HomeR)
   breadcrumb  _ = return ("home", Nothing)
 
 -- How to run database actions.
