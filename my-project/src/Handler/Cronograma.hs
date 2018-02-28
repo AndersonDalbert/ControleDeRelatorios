3bf37d741aa9b4ec9fc89d0cdd589fdb7fd4ca04
hukasu <lucas_fnf@hotmail.com>
2018-02-28 18:11:24 -0300

    Finish implementing Cronograma

--- my-project/src/Handler/Cronograma.hs
+++ my-project/src/Handler/Cronograma.hs
@@ -6,9 +6,11 @@
 module Handler.Cronograma where
 
 import Import
-import Data.Text (Text)
+import Text.Printf
+import Codec.Xlsx
+import qualified Data.ByteString.Lazy as L
+import Data.Dates
 
--- 	Monitores	Corretor	Data Inicio Correcao	Data Entrega Correcao	Links Video Aulas
 data ItemCronograma = ItemCronograma
     { itemid :: Text
     , nome :: Text
@@ -38,8 +40,55 @@ instance ToJSON ItemCronograma where
         , "links" .= links
         ]
 
+getRows :: Maybe CellMap -> [(Int, [(Int, Cell)])]
+getRows Nothing = []
+getRows (Just a) = toRows a
+
+dateToText :: DateTime -> Text
+dateToText (DateTime y m d h mm s) = pack (printf "%d-%02d-%02d %02d:%02d:%02d" y m d h mm s)
+
+getStringDateFromOA :: Double -> Text
+getStringDateFromOA oaDate = dateToText (addTime (addInterval oaZero (Days (truncate oaDays))) (Time (truncate oaHours) (truncate oaMinutes) (truncate oaSeconds)))
+    where
+        oaZero = (DateTime 1899 12 30 0 0 0)
+        oaDays = fromIntegral (floor oaDate)
+        oaHours = ((oaDate - oaDays) * 24)
+        oaMinutes = ((oaHours - fromIntegral (floor oaHours)) * 60)
+        oaSeconds = ((oaMinutes - fromIntegral (floor oaMinutes)) * 60)
+        
+getValue :: Maybe CellValue -> Text
+getValue Nothing = ""
+getValue (Just (CellText text)) = text
+getValue (Just (CellDouble oaDate)) = getStringDateFromOA oaDate
+getValue (Just (CellRich (RichTextRun _ text:_))) = text
+getValue _ = ""
+
+collectTexts :: [(Int, Cell)] -> [Text]
+collectTexts [] = []
+collectTexts ((_, (Cell _ Nothing _ _)):xs) = collectTexts xs
+collectTexts ((_, (Cell _ val _ _)):xs) = [getValue val] Prelude.++ collectTexts xs
+
+makeItem :: [Text] -> [ItemCronograma]
+makeItem [] = []
+makeItem [itemid, nome, descricao, liberacao, envio, envioatraso, monitores, corretor, correcao, resultado, links] =
+     [ItemCronograma itemid nome descricao liberacao envio envioatraso monitores corretor correcao resultado links]
+makeItem _ = []
+
+makeItems :: [(Int, [(Int, Cell)])] -> [ItemCronograma]
+makeItems [] = []
+makeItems ((_, row):xs) = (makeItem (collectTexts row)) Prelude.++ (makeItems xs)
+
+preMakeItems :: [(Int, [(Int, Cell)])] -> [ItemCronograma]
+preMakeItems [] = []
+preMakeItems rows = makeItems (Prelude.tail (Prelude.tail rows))
+
+extractRowsFromXlsx :: Xlsx -> [(Int, [(Int, Cell)])]
+extractRowsFromXlsx (Xlsx [] _ _ _ _) = []
+extractRowsFromXlsx (Xlsx ((_, (Worksheet _ _ cells _ _ _ _ _ _ _ _ _ _ _)):_) _ _ _ _) = toRows cells
+
 getCronogramaR :: Handler Value
-getCronogramaR = returnJson $ ItemCronograma "A" "B" "C" "2018-01-31 00:00:00" "2018-01-31 00:00:00" "2018-01-31 00:00:00" "D" "E" "2018-01-31 00:00:00" "2018-01-31 00:00:00" "F"
-
-
-    
+getCronogramaR = do
+    bs <- liftIO $ L.readFile "./data/cronograma.xlsx"
+    let xlsx = toXlsx bs
+    returnJson $ preMakeItems (extractRowsFromXlsx xlsx)
+        
